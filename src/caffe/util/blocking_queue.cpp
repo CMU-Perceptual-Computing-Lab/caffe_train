0cba003cb8c912c36fbb6672c4355dc0ba867925
xizero00 <xizero00@gmail.com>
2017-07-26 15:49:43 +0800

    modify Makefile to link with opencv_contrib and change blocking_queue.cpp to add two headers and Blockqueue with DataReader and Datum

--- src/caffe/util/blocking_queue.cpp
+++ src/caffe/util/blocking_queue.cpp
@@ -2,6 +2,8 @@
 #include <string>
 
 #include "caffe/layers/base_data_layer.hpp"
+#include "caffe/data_reader.hpp"
+#include "caffe/proto/caffe.pb.h"
 #include "caffe/parallel.hpp"
 #include "caffe/util/blocking_queue.hpp"
 
@@ -87,5 +89,7 @@ size_t BlockingQueue<T>::size() const {
 
 template class BlockingQueue<Batch<float>*>;
 template class BlockingQueue<Batch<double>*>;
+template class BlockingQueue<Datum*>;
+template class BlockingQueue<shared_ptr<DataReader::QueuePair> >;
 
 }  // namespace caffe
